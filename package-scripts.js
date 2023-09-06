58ea2f27b31fec785cc8fa8dffc9d3f1d8170c7d
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2023-09-06 16:25:04 +0200

    Initial commit

---
+++ package-scripts.js
@@ -0,0 +1,40 @@
+const input = Object.freeze({
+  mad: {
+    Main: `src/client/Main.mad`,
+  },
+  views: `src/client/`,
+  styles: `src/client/index.scss`,
+});
+
+const out = Object.freeze({
+  mad: {
+    Main: `build/public/js/bundle.js`,
+  },
+  styles: {
+    main: `build/public/styles/main.css`,
+  },
+  sitemap: "build/public/sitemap.xml",
+});
+
+module.exports = {
+  scripts: {
+    build: {
+      frontend: {
+        main: `madlib compile -i ${input.mad.Main} --target browser --bundle -o ${out.mad.Main}`,
+        watch: `madlib compile -i ${input.mad.Main} --target browser --bundle -o ${out.mad.Main} -w &`,
+        dev: "nps build.frontend.watch",
+      },
+    },
+    sync: {
+      description: "sync the browser",
+      script:
+        "browser-sync start --server build/public/ --files build/public/ --serveStatic build/public/ --no-open --reload-debounce 100",
+    },
+    dev: `concurrently ${[
+      `"copy-and-watch --watch src/client/**/*.{html,svg,json} build/public/"`,
+      `"sass --watch ${input.styles}:${out.styles.main}"`,
+      `"nps build.frontend.dev"`,
+      `"nps sync"`,
+    ].join(" ")}`,
+  },
+};
